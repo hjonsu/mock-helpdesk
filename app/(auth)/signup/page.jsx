837f71201b34a587c59d04a7af96e72b933976d1
hjonsu <jsu6898@gmail.com>
2023-11-22 08:29:21 -0500

    group routes for organization

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SignUp() {
+  return (
+    <main>
+      <h2 className="text-center">Sign Up</h2>
+    </main>
+  );
+}
+
+// /auth/signup
+// wrapping auth folder in () will remove it from route
