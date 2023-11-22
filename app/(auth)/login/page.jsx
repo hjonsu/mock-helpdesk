fbd9a4377bec8065a9e803f9f66fca760eecd55a
hjonsu <jsu6898@gmail.com>
2023-11-22 12:38:49 -0500

    supabase int & signup/login form

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,19 @@
+"use client";
+
 import React from "react";
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+
+    console.log("login", email, password);
+  };
+
   return (
     <main>
       <h2 className="text-center">Login</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
+import AuthForm from "../AuthForm";
 
 export default function SignUp() {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+
+    console.log("signup", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 }
