c3cc8293628ba3dd7a904b0dd2f0f8727ddfcd8b
hjonsu <jsu6898@gmail.com>
2023-11-22 09:03:03 -0500

    dynamic metadata

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
+export const metadata = {
+  title: "Mock Helpdesk | Tickets",
+  description: "Tickets page of a Helpdesk app created by Jonathan Su (hjonsu)",
+};
+
 export default function DashboardLayout({ children }) {
   return (
     <>
