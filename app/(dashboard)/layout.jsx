70525ccb5b9894df88e03c4f4bf7ae69c4e32859
hjonsu <jsu6898@gmail.com>
2023-11-23 19:47:18 -0500

    redirect based on auth

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 import Navbar from "../components/Navbar";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Mock Helpdesk | Tickets",
@@ -12,7 +13,9 @@ export default async function DashboardLayout({ children }) {
   const supabase = createServerComponentClient({ cookies });
 
   const { data } = await supabase.auth.getSession();
-  console.log(data.session, "data");
+
+  !data.session ? redirect("/login") : null;
+
   return (
     <>
       <Navbar prop={data.session.user} />
