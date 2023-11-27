07bb7fee2bb89d54d54eb20c549620d897de115d
hjonsu <jsu6898@gmail.com>
2023-11-27 11:00:35 -0500

    create form server action

--- app/(dashboard)/tickets/create/CreateForm.jsx
+++ app/(dashboard)/tickets/create/CreateForm.jsx
@@ -1,50 +1,52 @@
 "use client";
 
+import { addTicket } from "@/app/actions";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function CreateForm() {
   const router = useRouter();
 
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [priority, setPriority] = useState("low");
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+  // const [title, setTitle] = useState("");
+  // const [body, setBody] = useState("");
+  // const [priority, setPriority] = useState("low");
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setIsLoading(true);
 
-    const newTicket = {
-      title,
-      body,
-      priority,
-    };
+  //   const newTicket = {
+  //     title,
+  //     body,
+  //     priority,
+  //   };
 
-    const res = await fetch("http://localhost:3000/api/tickets", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTicket),
-    });
+  //   const res = await fetch("/api/tickets", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(newTicket),
+  //   });
 
-    const json = await res.json();
+  //   const json = await res.json();
 
-    if (json.error) {
-      console.log(error.message);
-    }
-    if (json.data) {
-      router.refresh();
-      router.push("/tickets");
-    }
-  };
+  //   if (json.error) {
+  //     console.log(error.message);
+  //   }
+  //   if (json.data) {
+  //     router.refresh();
+  //     router.push("/tickets");
+  //   }
+  // };
 
   return (
-    <form onSubmit={handleSubmit} className="w-1/2">
+    <form action={addTicket} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
           required
           type="text"
+          name="title"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
         />
@@ -53,21 +55,25 @@ export default function CreateForm() {
         <span>Body:</span>
         <textarea
           required
+          name="body"
           onChange={(e) => setBody(e.target.value)}
           value={body}
         />
       </label>
       <label>
         <span>Priority:</span>
-        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+        <select
+          onChange={(e) => setPriority(e.target.value)}
+          value={priority}
+          name="priority"
+        >
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
         </select>
       </label>
       <button className="btn-primary" disabled={isLoading}>
-        {isLoading && <span>Adding...</span>}
-        {!isLoading && <span>Add Ticket</span>}
+        {isLoading ? <span>Adding...</span> : <span>Add Ticket</span>}
       </button>
     </form>
   );
