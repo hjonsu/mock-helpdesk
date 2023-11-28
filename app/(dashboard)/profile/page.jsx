ae1965a4dd8579e976c8d49aedf195bbe954e9b9
hjonsu <jsu6898@gmail.com>
2023-11-28 09:34:35 -0500

    Add profile & style changes

--- app/(dashboard)/profile/page.jsx
+++ app/(dashboard)/profile/page.jsx
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function page() {
   const supabase = createServerComponentClient({ cookies });
@@ -19,15 +20,29 @@ export default async function page() {
   }
 
   return (
-    <main>
-      <h2>Profile: {session.user.email}</h2>
-      <h3>First Name:</h3>
-      <p>{data.first_name}</p>
-      <h3>Last Name:</h3>
-      <p>{data.last_name}</p>
-      <p>Account info is in here.</p>
-
-      <button>Update Information</button>
+    <main className="text-center flex flex-col justify-center items-center gap-4">
+      <div className="card">
+        <div className="flex flex-col gap-4 w-80">
+          <div className="flex flex-row w-full justify-center">
+            <h2>Profile: </h2>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <h3>First Name: </h3>
+            <p className="mx-4">{data.first_name}</p>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <h3>Last Name: </h3>
+            <p className="mx-4">{data.last_name}</p>
+          </div>
+          <div className="flex flex-row w-full items-center justify-between">
+            <h3>Email: </h3>
+            <p className="mx-4">{session.user.email}</p>
+          </div>
+          <Link href="/profile/update" className="btn-primary p-3">
+            Edit Info
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
