5601bf4bf022a53cd267543b0d8ec7b42cf18d8b
hjonsu <jsu6898@gmail.com>
2023-12-04 14:58:22 -0800

    styling nav in lg screens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = (user) => {
     <div className="fixed top-0 left-0 right-0 bg-neutral-950 border-b border-neutral-700 z-10 px-5 md:bg-transparent">
       <nav
         id="nav"
-        className="flex items-center justify-between w-full md:justify-start py-3 lg:py-5"
+        className="flex items-center justify-between w-full md:justify-start py-3 lg:py-5 max-w-5xl mx-auto"
       >
         <Link href="/" className="mx-2 hover:scale-110 duration-150">
           <Image src={Logo} alt="Mock helpdesk logo" width={40} quality={100} />
